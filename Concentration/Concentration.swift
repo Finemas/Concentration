5813c022ef81f0f4fecb6e326f807fb6018276c6
Jan Provazník <proovaznik@gmail.com>
2020-03-05 12:15:35 +0100

    The second Standford lecture

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,49 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Jan Provazník on 05/03/2020.
+//  Copyright © 2020 Jan Provazník. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        cards[index].isFaceUp.toggle()
+        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // chech if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no  cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberofPairsOfCards: Int) {
+        for _ in 0..<numberofPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        
+        // TODO: Shuffle the cards
+    }
+}

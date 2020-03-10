c9472fe6339efd6e0d175f5bf8ce8abca59d5dc0
Jan Provazník <proovaznik@gmail.com>
2020-03-10 11:44:21 +0100

    the seventh Standford lecture
    Topics were Timer. Animations and Mutliple MVC - SplitViewController, TabBarController, NavigationController and Segues

---
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ConcentrationThemeChooserViewController.swift
+//  Concentration
+//
+//  Created by Jan Provazník on 10/03/2020.
+//  Copyright © 2020 Jan Provazník. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
+    
+    let themes = [
+        "Sports": "⚽️🏀🏉⚾️🎾🏐🏈🎱🏓🏂🎳⛳️🪀🛹🧘🥏",
+        "Animals": "🐶🐔🦊🐼🦀🐪🐓🐋🐙🦄🐵🐿🦔🦧🐢",
+        "Faces": "😃😂😎😫😰😴🙄🤔🥰😷🥶🥳🤪🤢",
+        "Halloween": "🦇🧛🏻🧟😈🎃👻🍫🔪🕷👽"
+    ]
+    
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true // I did it but I'm actually not going to do it
+            }
+        }
+        return false // I didn't collapse it so you should do it
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName]  {
+                cvc.theme = theme
+            }
+        } else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName]  {
+                 cvc.theme = theme
+             }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+    
+    // MARK: - Navigation
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName]  {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
+                }
+            }
+        }
+    }
+
+}
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConcentrationViewController: UIViewController {
 
     @IBOutlet private weak var flipCountLabel: UILabel! {
         didSet {
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     private func updateFlipCountLabel() {
         let attributes: [NSAttributedString.Key: Any] = [
             .strokeWidth: 5,
-            .strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            .strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         ]
         
         let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
@@ -55,17 +55,19 @@ class ViewController: UIViewController {
     }
     
     private func updateViewFromModel() {
-        for index in cardButtons.indices {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            
-            if card.isFaceUp {
-                button.setTitle(emoji(for: card), for: .normal)
-                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            } else {
-                button.setTitle("", for: .normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-                button.isEnabled = !card.isMatched
+        if cardButtons != nil {
+            for index in cardButtons.indices {
+                let button = cardButtons[index]
+                let card = game.cards[index]
+                
+                if card.isFaceUp {
+                    button.setTitle(emoji(for: card), for: .normal)
+                    button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+                } else {
+                    button.setTitle("", for: .normal)
+                    button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+                    button.isEnabled = !card.isMatched
+                }
             }
         }
     }
@@ -90,13 +92,21 @@ class ViewController: UIViewController {
         
         for button in cardButtons {
             button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
             button.isEnabled = true
         }
         
         game = Concentration(numberofPairsOfCards: numberOfPairsOfCards)
         
-        emojiChoices = "🦇🧛🏻🧟😈🎃👻🍫🔪🕷👽"
+        emojiChoices = theme ?? ""
+    }
+    
+    var theme: String? {
+        didSet {
+            emojiChoices = theme ?? ""
+            emoji = [:]
+            updateViewFromModel()
+        }
     }
     
     private var emojiChoices = "🦇🧛🏻🧟😈🎃👻🍫🔪🕷👽"
